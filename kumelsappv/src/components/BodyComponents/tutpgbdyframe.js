9fab6c8daa0213168813c7bbb4b41a182548723e
Juan Soberanes <45519261+johnfsu@users.noreply.github.com>
2020-10-19 14:40:23 -0700

    Small update
    Nav Update

--- kumelsappv/src/components/BodyComponents/tutpgbdyframe.js
+++ kumelsappv/src/components/BodyComponents/tutpgbdyframe.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import {Nav,Button} from 'react-bootstrap';
-import '../../Assets/css/tutoriallist.css';
+import '../../Assets/css/tutbdyframe.css';
+
 
 export const Tutpgbdyframe = () => (
+    <>
+        <div class="container">
+            <div class="card bg-primary text-white yes">
+                <div class="card-body">Primary card</div>
+            </div>
+       </div>
 
-
+    </>
    )
 
 export default Tutpgbdyframe;
--- kumelsappv/src/components/Pages/Linuxpage.js
+++ kumelsappv/src/components/Pages/Linuxpage.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {Tutoriallist} from '../BodyComponents/tutoriallist'
+import {Tutpgbdyframe} from '../BodyComponents/tutpgbdyframe'
+
+import '../../Assets/css/tutbody.css';
 
 /* import TweetEmbed from 'react-tweet-embed' */
 
 function Linux()
     {
         return(
-            <div>
 
-                <h1>Linux Tutorials!</h1>
-            </div>
+            <>
+                <div>
+
+                    <h1>Linux Tutorials!</h1>
+                </div>
+
+                <Tutoriallist/>
+                <Tutpgbdyframe/>
+           </>
 
 
         );
